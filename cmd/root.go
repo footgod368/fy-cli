fb38bce18bfd2efe06f6bcb0e76b5ba12d196584
libole <libole@bytedance.com>
2025-11-23 02:26:37 +0800

    1

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "fy-cli",
+	Use:   "fy",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -20,8 +20,7 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Args: cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return root.RunE(args)
 	},
