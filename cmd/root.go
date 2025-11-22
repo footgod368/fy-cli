c863c8f55946f3028e422e8688122fbe95a9a606
libole <libole@bytedance.com>
2025-11-23 02:29:09 +0800

    strings.Join(args, " ")

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "fy",
+	Use:   "fy [text]",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
--- internal/cmd/root/run.go
+++ internal/cmd/root/run.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	youdao "github.com/footgod368/translator-sdk"
 	"github.com/footgod368/translator-sdk/utils"
+	"strings"
 )
 
 func RunE(args []string) error {
 	ctx := context.Background()
-	resp, err := youdao.Query(ctx, args[0])
+	resp, err := youdao.Query(ctx, strings.Join(args, " "))
 	if err != nil {
 		return err
 	}
