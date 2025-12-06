c2596f8b08c313d68b531d56b8655c2b4304bc11
Bole Li <boleli@qq.com>
2025-12-06 16:35:04 +0800

    desc

--- cmd/root.go
+++ cmd/root.go
@@ -4,23 +4,26 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"os"
+
 	"github.com/footgod368/fy-cli/internal/cmd/common"
 	"github.com/footgod368/fy-cli/internal/cmd/root"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
+const longDescription = `fy 是一个命令行英文词典工具，支持查询单词释义、同义词辨析等功能。
+示例：
+- 查询单词：$ fy insist
+- 查询单词及其同义词辨析：$ fy -d insist
+- 查看帮助：$ fy -h
+`
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "fy [text]",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "命令行英文词典",
+	Long:  longDescription,
 	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		return root.Complete(toComplete)
 	},
